54a0d99c235f179023e57daf88e261c2473b3081
Hassan Khan <contact@hassankhan.me>
2016-05-07 12:30:36 +0100

    Tidied up generator code massively

--- index.js
+++ index.js
@@ -3,74 +3,101 @@ const path = require('path');
 const generator = require('yeoman-generator');
 const chalk = utils.chalk;
 
+const install = {
+    templates: [{
+        src: ['src', '**'],
+        dest: ['android'],
+    }],
+    files: [{
+        src: ['bin', '**'],
+        dest: ['android'],
+    }],
+};
+
+const upgrade = {
+    templates: [{
+        src: ['src', '*'],
+        dest: ['android'],
+    }, {
+        src: ['src', 'app', '*'],
+        dest: ['android', 'app'],
+    }],
+    files: [],
+};
+
 module.exports = generator.Base.extend({
-  constructor: function() {
-    generator.Base.apply(this, arguments);
-    this.argument('name', { type: String, required: true });
+    constructor: function() {
+        generator.Base.apply(this, arguments);
+        this.argument('name', { type: String, required: true });
 
-    this.option('package', {
-      desc: 'Package name for the application (com.example.app)',
-      type: String,
-      defaults: 'com.' + this.name.toLowerCase()
-    });
-    // this.option('upgrade', {
-    //   desc: 'Specify an upgrade',
-    //   type: Boolean,
-    //   defaults: false
-    // });
-  },
+        this.option('package', {
+            desc: 'Package name for the application (com.example.app)',
+            type: String,
+            defaults: 'com.' + this.name.toLowerCase(),
+        });
+        this.option('upgrade', {
+          desc: 'Upgrade the application',
+          type: Boolean,
+          defaults: false,
+        });
+    },
 
-  initializing: function() {
-    if (!utils.isValidPackageName(this.options.package)) {
-      throw new Error('Package name ' + this.options.package + ' is not valid');
-    }
-  },
+    initializing: function() {
+        if (!utils.validate.isPackageName(this.options.package)) {
+            throw new Error('Package name ' + this.options.package + ' is not valid');
+        }
+    },
 
-  writing: function() {
-    var templateParams = {
-      package: this.options.package,
-      name: this.name
-    };
-    // if (!this.options.upgrade) {
-      this.fs.copyTpl(
-        this.templatePath(path.join('src', '**')),
-        this.destinationPath('android'),
-        templateParams
-      );
-      this.fs.copy(
-        this.templatePath(path.join('bin', '**')),
-        this.destinationPath('android')
-      );
-    // } else {
-    //   this.fs.copyTpl(
-    //     this.templatePath(path.join('src', '*')),
-    //     this.destinationPath('android'),
-    //     templateParams
-    //   );
-    //   this.fs.copyTpl(
-    //     this.templatePath(path.join('src', 'app', '*')),
-    //     this.destinationPath(path.join('android', 'app')),
-    //     templateParams
-    //   );
-    // }
+    writing: function() {
+        const templateParams = {
+            package: this.options.package,
+            name: this.name,
+        };
 
-    var javaPath = path.join.apply(
-      null,
-      [process.cwd(), 'android', 'app', 'src', 'main', 'java'].concat(this.options.package.split('.'))
-    );
+        const copyTemplate = (template) => {
+            this.fs.copyTpl(
+                this.templatePath(path.join(...template.src)),
+                this.destinationPath(path.join(...template.dest)),
+                templateParams
+            );
+        };
 
-    this.fs.copyTpl(
-      this.templatePath(path.join('package', '**')),
-      this.destinationPath(javaPath),
-      templateParams
-    );
-  },
+        const copyFile = (file) => {
+            this.fs.copy(
+                this.templatePath(path.join(...file.src)),
+                this.destinationPath(path.join(...file.dest)),
+                templateParams
+            );
+        };
 
-  end: function() {
-    var projectPath = this.destinationRoot();
-    this.log(chalk.white.bold('To run your app on Android:'));
-    this.log(chalk.white('   Have an Android emulator running (quickest way to get started), or a device connected'));
-    this.log(chalk.white('   cd ' + projectPath));
-    this.log(chalk.white('   react-native run-android'));
-  }
+        if (!this.options.upgrade) {
+            // If installing
+            Object.keys(install).forEach((resource) => {
+                install.templates.forEach(copyTemplate);
+                install.files.forEach(copyFile);
+            });
+        } else {
+            // If upgrading
+            Object.keys(upgrade).forEach((resource) => {
+                upgrade.templates.forEach(copyTemplate);
+                upgrade.files.forEach(copyFile);
+            });
+        }
+
+        const javaPath = path.join(...[this.destinationRoot(), 'android', 'app', 'src', 'main', 'java'].concat(this.options.package.split('.'))
+        );
+
+        this.fs.copyTpl(
+            this.templatePath(path.join('package', '**')),
+            this.destinationPath(javaPath),
+            templateParams
+        );
+    },
+
+    end: function() {
+        this.log(chalk.white.bold('To run your app on Android:'));
+        this.log(chalk.white('   Have an Android emulator running (quickest way to get started), or a device connected'));
+        this.log(chalk.white('   cd ' + this.destinationRoot()));
+        this.log(chalk.white('   rsx run android'));
+    },
 });
